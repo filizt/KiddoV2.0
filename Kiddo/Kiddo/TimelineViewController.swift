b0b34b6e8a9471804dab35642606be7341215e2c
mikemiksch <mikemiksch@gmail.com>
2018-01-10 10:05:12 -0800

    Added appID to take the user to the correct app on the app store. Built the code out in a separate app to test it, and it appears to be functioning as intended.

--- Kiddo/Kiddo/TimelineViewController.swift
+++ Kiddo/Kiddo/TimelineViewController.swift
@@ -895,7 +895,7 @@ class TimelineViewController: UIViewController, UITableViewDataSource, UITableVi
     func presentFeedbackAlert() {
         let alert = UIAlertController(title: "Enjoying Kiddo App?", message: "Recommend Kiddo to others by leaving us a review on the App Store.", preferredStyle: .alert)
         let rate = UIAlertAction(title: "Yes, rate it now", style: .default) { (action) in
-            let appID = String() // come back to this with the actual appID
+            let appID = "1210910332"
             if let appStoreURL = URL(string: "itms-apps://itunes.apple.com/app/viewContentsUserReviews?id=\(appID)") {
                 UIApplication.shared.open(appStoreURL, options: [:], completionHandler: nil)
             }
